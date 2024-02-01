a2e922335206509123c2d00f2c5c63c5a450ab1c
jinanmh123 <147313181+jinanmh123@users.noreply.github.com>
2024-02-01 14:47:51 +0600

    Update key_gen.py
    Modified the key_gen script

--- key_gen.py
+++ key_gen.py
@@ -1,19 +1,17 @@
 from cryptography.fernet import Fernet
-import secrets
-import base64
+import bcrypt
 
-def generate_access_key():
-    # Generate a random key
-    key = Fernet.generate_key()
+def generate_access_key():  # generate the software license/product key
+    key = Fernet.generate_key() # a strong random key is generated
+    key_str = str(key).removeprefix("b'").removesuffix("='")
+    return key_str
 
-    # Apply the fernet algorithm
-    cipher_suite = Fernet(key)
-
-    # Generate the random access key (combination of numbers and letters as you desired)
-    access_key = base64.urlsafe_b64encode(cipher_suite.encrypt(secrets.token_bytes(32))).decode('utf-8')
-
-    return access_key
+def hash_key(key):
+    hashed_key= bcrypt.hashpw(key.encode('utf-8'), bcrypt.gensalt()) # hash the access key using bcrypt
+    return hashed_key
 
 if __name__ == "__main__":
     access_key = generate_access_key()
-    print("Generated Key:", access_key)
+    hash_of_key = hash_key(access_key) # you can store this hash in the database
+    print(f"Generated key : {access_key}")
+    # print(hash_of_key)
